8c9b998822f7137197fe2139a15eb016aa1ceb37
Abhijeet <abhijeetkolhe38@gmail.com>
2026-02-20 01:18:05 +0530

    initialization

---
+++ eventseat/src/test/java/com/abhijeet/eventseat/EventseatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijeet.eventseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventseatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
